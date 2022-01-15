44a5ca6b5929d0b41292951d9b05bc71d0e8dc60
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2022-01-15 09:55:08 +0600

    solved meili error

--- addmovies.js
+++ addmovies.js
@@ -3,7 +3,10 @@ const movies = require("./movies.json");
 
 client = new MeiliSearch({
     host: "http://eumpstdziw.us05.qoddiapp.com/",
-    apiKey: "masterkey",
+    apiKey: "masterKey",
+    headers: {
+        "X-Meili-API-Key": process.env.MEILI_MASTER_KEY,
+    },
 });
 
 client.index("movies").addDocuments(movies);
